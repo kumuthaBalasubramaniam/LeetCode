a2f50b31d08f3708d24c3c023e7a79685f5caa88
Kumutha Balasubramaniam <166600667+kumuthaBalasubramaniam@users.noreply.github.com>
2024-08-17 10:40:41 +0530

    Time: 9 ms (52.69%) | Memory: 44.6 MB (64.42%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -1,11 +1,41 @@
 class Solution {
-  public String shortestPalindrome(String s) {
-    final String t = new StringBuilder(s).reverse().toString();
 
-    for (int i = 0; i < t.length(); ++i)
-      if (s.startsWith(t.substring(i)))
-        return t.substring(0, i) + s;
+    public static String shortestPalindrome(String s) {
+        // Reverse the given string
+        String rev = new StringBuilder(s).reverse().toString();
 
-    return t + s;
-  }
+        // Combine the original string with its reverse
+        String combined = s + "#" + rev;
+
+        // Compute the prefix array for the combined string
+        int[] prefix = computePrefix(combined);
+
+        // The value at the last index of the prefix array tells us
+//the length of the
+        // longest palindrome prefix of the original string.
+        int palindromeLength = prefix[prefix.length - 1];
+
+        // Append the non-palindromic suffix from the reverse to the
+//front of the original string
+        return rev.substring(0, s.length() - palindromeLength) + s;
+    }
+
+    // Helper method to compute the prefix array (KMP algorithm)
+    private static int[] computePrefix(String s) {
+        int[] prefix = new int[s.length()];
+        int j = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+            while (j > 0 && s.charAt(i) != s.charAt(j)) {
+                j = prefix[j - 1];
+            }
+            if (s.charAt(i) == s.charAt(j)) {
+                j++;
+            }
+            prefix[i] = j;
+        }
+        return prefix;
+    }
+
+
 }
